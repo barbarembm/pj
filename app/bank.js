c7bca9f2927dfb61e02c24ae04ecfc93e760f9ed
LAB316 Student21 <LAB316_Student21@gau.local>
2020-01-13 13:16:48 +0400

    pj

---
+++ app/bank.js
@@ -0,0 +1,69 @@
+
+
+
+function mail()  
+{  
+var x=document.sup.email.value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+  return false;  
+  }  
+}  
+
+
+function checkf(){
+ 
+    var firstpassword=document.sup.password.value;  
+    var secondpassword=document.sup.password2.value;  
+      
+    if(firstpassword==secondpassword){  
+    return true;  
+    }  
+    else{  
+    alert("password must be same!");  
+    return false;  
+    }  
+  
+
+}
+
+function validate() {
+      
+  if( document.sup.name.value == "" ) {
+     alert( "Please provide your name!" );
+     document.sup.Name.focus() ;
+     return false;
+  }
+}
+
+function validate() {
+      
+  if( document.sup.coun.value == "" ) {
+     alert( "Please provide your name!" );
+     document.sup.Name.focus() ;
+     return false;
+  }
+}
+function validate() {
+      
+  if( document.sup.city.value == "" ) {
+     alert( "Please provide your name!" );
+     document.sup.Name.focus() ;
+     return false;
+  }
+}
+
+function getInputValue(){
+  // Selecting the input element and get its value 
+  var inputVal = document.getElementById("myInput").value;
+  if(inputVal.length >= 9){
+    console.log("aris");
+  }else {
+    console.log("ar aris")
+  }
+  // Displaying the value
+  alert(inputVal);
+}
+
